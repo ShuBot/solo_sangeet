99246066ed1207f16e1dc2c04071700a556814f4
Shubham Mohapatra <01shubham10@gmail.com>
2026-05-31 23:37:17 +0530

    Updates main with test_lvgl option to test various LVGL features. Testing Lottie Player now.

---
+++ firmware/solo_sangeet/main/include/solo_sangeet.h
@@ -0,0 +1,13 @@
+// solo_sangeet.h
+#ifndef SOLO_SANGEET_H
+#define SOLO_SANGEET_H
+
+#include "lvgl.h"
+// #include "ili9341_driver.h"
+// #include "xpt2046_touch_driver.h"
+// #include "ui_theme.h"
+// #include "audio_player.h"
+
+void test_ui_init(lv_disp_t *disp);
+
+#endif /* SOLO_SANGEET_H */
